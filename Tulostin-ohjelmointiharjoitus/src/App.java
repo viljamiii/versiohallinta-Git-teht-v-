d6829f19ab269030e414e31f031a1db1e8be662b
viljamiii <viljami20102@student.hamk.fi>
2024-10-25 12:00:50 +0300

    Create App.java

---
+++ Tulostin-ohjelmointiharjoitus/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Hei olen Tulostin-ohjelma
+        double luku1 = 10; 
+        double luku2 = 30;
+        double tulo = luku1 * luku2;
+        double erotus = luku1 - luku2;
+        double summa = luku1 + luku2;
+        double jako = luku1 / luku2;
+        System.out.println("Hei olen Tulostin-ohjelma");
+        String tekija;
+        tekija = "Viljami Ahola";
+        System.out.print("Ohjelman tekijä ");
+        System.out.println(tekija);
+        System.out.println("luku1-muuttujan arvo on " + luku1);
+        System.out.println("luku2-muuttujan arvo on " + luku2);
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+        System.out.println("luku1-muuttujan arvo on " + luku1);
+        System.out.println("luku2-muuttujan arvo on " + luku2);
+        System.out.println(luku1 + " * " + luku2 + " = " + jako);
+
+
+
+    }
+}
